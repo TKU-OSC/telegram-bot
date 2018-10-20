33a6d07ef0275ecc27cb79251f4e9f24801c23e4
isekai <brian1689878638@gmail.com>
2018-10-20 23:32:47 +0800

    Fix the packages and files on the setup.py and change the version code.

--- setup.py
+++ setup.py
@@ -5,13 +5,18 @@ with open('requirements.txt', 'r') as requirements:
 
 setup(
     name='TKUOSC_OrderBot',
-    version='0.0.isekai_dev0',
+    version='0.1.isekai_dev0',
     packages=['tkuosc_bot',
-              'tkuosc_bot.instructions'
+              'tkuosc_bot.instructions',
+              'tkuosc_bot.instructions.conversations',
+              'tkuosc_bot.data_base',
+              'tkuosc_bot.utils',
               ],
     install_requires=install_requires,
     python_requires='>=3',
-    data_files=[('files', ['files/Ai_cafe_drinks.json'])],
+    data_files=[('files/menu', ['files/menu/Ai_cafe_drinks.json']),
+                ('files/meet', [])
+                ],
     url='https://github.com/TKU-OSC/telegram-bot/tree/master',
     license='MIT',
     author='TKU-OSC Members',
